8a7cab959f308ea4218622b9c1af6fc1b44f5ae1
unexpectedhaki <thomas.pritchard20@gmail.com>
2022-12-02 16:15:57 -0500

    Javascript Update
    Edited the event listener to return converted values based on the units.
    Ready for CSS styling.

--- index.js
+++ index.js
@@ -3,29 +3,22 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
-
-let units = [
-    {
-        meters: inputEl.value,
-        feet: inputEl.value
-    },
-    {
-        liters: inputEl.value,
-        gallons: inputEl.value
-    },
-    {
-        kilos: inputEl.value,
-        pounds: inputEl.value
-    }
-]
-
 const inputEl = document.getElementById("input-field")
 const convertBtn = document.getElementById("convert-btn")
+const lengthEl = document.getElementById("length-el")
+const volumeEl = document.getElementById("volume-el")
+const massEl = document.getElementById("mass-el")
 
-function convert() {
-    
-}
+const meterToFeet = 3.281
+const literToGallon = 0.264
+const kiloToPound = 2.204
 
 convertBtn.addEventListener("click", function() {
-    convert()
+    let baseValue = inputEl.value
+
+    lengthEl.textContent = `${baseValue} meters = ${(baseValue * meterToFeet).toFixed(3)} feet | ${baseValue} feet = ${(baseValue / meterToFeet).toFixed(3)} meters`
+
+    volumeEl.textContent = `${baseValue} liters = ${(baseValue * literToGallon).toFixed(3)} gallons | ${baseValue} gallons = ${(baseValue / literToGallon).toFixed(3)} liters`
+
+    massEl.textContent = `${baseValue} kilos = ${(baseValue * kiloToPound).toFixed(3)} pounds | ${baseValue} pounds = ${(baseValue / kiloToPound).toFixed(3)} kilos`
 })
